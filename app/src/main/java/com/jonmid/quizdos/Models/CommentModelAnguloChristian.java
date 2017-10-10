34a0dc7d830778106862c0b5ee77d690dbe7a436
christian <christiancamilo1221@gmail.com>
2017-10-10 12:39:08 -0500

    c1

--- app/src/main/java/com/jonmid/quizdos/Models/CommentModelAnguloChristian.java
+++ app/src/main/java/com/jonmid/quizdos/Models/CommentModelAnguloChristian.java
@@ -5,4 +5,41 @@ package com.jonmid.quizdos.Models;
  */
 
 public class CommentModelAnguloChristian {
+
+    private String urlimage_commnet;
+    private String name;
+    private String email;
+    private String body;
+
+    public String getUrlimage_commnet() {
+        return urlimage_commnet;
+    }
+
+    public void setUrlimage_commnet(String urlimage_commnet) {
+        this.urlimage_commnet = urlimage_commnet;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
--- app/src/main/java/com/jonmid/quizdos/Models/UserModelAnguloChristian.java
+++ app/src/main/java/com/jonmid/quizdos/Models/UserModelAnguloChristian.java
@@ -5,4 +5,68 @@ package com.jonmid.quizdos.Models;
  */
 
 public class UserModelAnguloChristian {
+
+    private String urlimage_user;
+    private String name;
+    private String username;
+    private String email;
+    private String phone;
+    private String namecompany;
+    private String address;
+
+    public String getUrlimage_user() {
+        return urlimage_user;
+    }
+
+    public void setUrlimage_user(String urlimage_user) {
+        this.urlimage_user = urlimage_user;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getNamecompany() {
+        return namecompany;
+    }
+
+    public void setNamecompany(String namecompany) {
+        this.namecompany = namecompany;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
