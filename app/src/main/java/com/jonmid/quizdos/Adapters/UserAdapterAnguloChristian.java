5bd06497b1fbbba99221a8a7df2ee6df3c65e098
christian <christiancamilo1221@gmail.com>
2017-10-10 12:34:22 -0500

    c1

---
+++ app/src/main/java/com/jonmid/quizdos/Adapters/UserAdapterAnguloChristian.java
@@ -0,0 +1,8 @@
+package com.jonmid.quizdos.Adapters;
+
+/**
+ * Created by CHRISTIAN on 10/10/2017.
+ */
+
+public class UserAdapterAnguloChristian {
+}
---
+++ app/src/main/java/com/jonmid/quizdos/Parser/CommentJsonAnguloChristian.java
@@ -0,0 +1,8 @@
+package com.jonmid.quizdos.Parser;
+
+/**
+ * Created by CHRISTIAN on 10/10/2017.
+ */
+
+public class CommentJsonAnguloChristian {
+}
