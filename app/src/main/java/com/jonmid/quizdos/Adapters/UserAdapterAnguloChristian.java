3fa2e3747438ce5b1ce1be4fac2532526f6d33e7
christian <christiancamilo1221@gmail.com>
2017-10-10 13:38:50 -0500

    c4

--- app/src/main/java/com/jonmid/quizdos/Adapters/UserAdapterAnguloChristian.java
+++ app/src/main/java/com/jonmid/quizdos/Adapters/UserAdapterAnguloChristian.java
@@ -1,12 +1,18 @@
 package com.jonmid.quizdos.Adapters;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.jonmid.quizdos.Models.UserModelAnguloChristian;
 import com.jonmid.quizdos.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by CHRISTIAN on 10/10/2017.
@@ -14,20 +20,36 @@ import com.jonmid.quizdos.R;
 
 public class UserAdapterAnguloChristian extends RecyclerView.Adapter<UserAdapterAnguloChristian.ViewHolder>{
 
+    List<UserModelAnguloChristian> userList = new ArrayList<>();
+    Context context;
+
+    public UserAdapterAnguloChristian(List<UserModelAnguloChristian> userList, Context context) {
+        this.userList = userList;
+        this.context = context;
+    }
 
     @Override
+
+
     public UserAdapterAnguloChristian.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View item = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_user, parent, false);
+
+        // Pasar la vista (item.xml) al ViewHolder
+        ViewHolder viewHolder = new ViewHolder(item);
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(UserAdapterAnguloChristian.ViewHolder holder, int position) {
+        holder.name.setText(userList.get(position).getName());
+
+
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return userList.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
